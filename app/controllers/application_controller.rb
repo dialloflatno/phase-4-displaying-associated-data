db8c72c01527ff5e2674af250ddd099b9ef12dad
dialloflatno <brahimdiallo23@gmail.com>
2021-12-22 11:48:20 -0500

    commit tree b246df4ebb0508d89550eb53491cbc94d0d216d4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+    before_action :dog_house,only:[:show]
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+
 end
+ 
+private
+
+#RESCUE------------------------------------------------------------------------
+
+    def render_not_found_response
+        render json: { error: "Dog house not found" }, status: :not_found
+    end
+    
+#Dog_house------------------------------------------------------------------------
+
+    def dog_house 
+        DogHouse.find(params[:id])
+    end
--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -1,15 +1,7 @@
 class DogHousesController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def show
-    dog_house = DogHouse.find(params[:id])
-    render json: dog_house
-  end
-
-  private
-
-  def render_not_found_response
-    render json: { error: "Dog house not found" }, status: :not_found
+    render json: dog_house, include: :reviews
   end
 
 end
